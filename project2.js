0fff8c50e6da1cd527bd9ca067c5311556c8a973
ItsWadex <bouassida.mohamedaziz@gmail.com>
2024-03-06 09:48:08 +0100

    updated the code

--- project2.js
+++ project2.js
@@ -1,102 +1,99 @@
 function reverseString(str) {
-  return str.split("").reverse().join("")
+  return str.split("").reverse().join("");
 }
-console.log(reverseString("hello"))
+console.log(reverseString("hello"));
 
 function countCharacters(str) {
-  return str.length
+  return str.length;
 }
-console.log(countCharacters("hello"))
+console.log(countCharacters("hello"));
 
 function capitalizeWords(sentence) {
-    let capitalized = ''
-    let capitalizeNext = true
-    for (let i = 0; i < sentence.length; i++) {
-        if (capitalizeNext && sentence[i] !== ' ') {
-            capitalized += sentence[i].toUpperCase()
-            capitalizeNext = false;
-        } else {
-            capitalized += sentence[i]
-        }
-        if (sentence[i] === ' ') {
-            capitalizeNext = true
-        }
+  let capitalized = "";
+  let capitalizeNext = true;
+  for (let i = 0; i < sentence.length; i++) {
+    if (capitalizeNext && sentence[i] !== " ") {
+      capitalized += sentence[i].toUpperCase();
+      capitalizeNext = false;
+    } else {
+      capitalized += sentence[i];
     }
-    return capitalized
+    if (sentence[i] === " ") {
+      capitalizeNext = true;
+    }
+  }
+  return capitalized;
 }
 
-console.log(capitalizeWords("hello world"))
+console.log(capitalizeWords("hello world"));
 
 function findMax(arr) {
-    let max = arr[0];
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] > max) {
-            max = arr[i]
-        }
+  let max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
     }
-    return max;
+  }
+  return max;
 }
 
 function findMin(arr) {
-    let min = arr[0];
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] < min) {
-            min = arr[i]
-        }
+  let min = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
     }
-    return min;
+  }
+  return min;
 }
-console.log(findMax([3, 5, 2, 8, 1]))
-console.log(findMin([3, 5, 2, 8, 1]))
+console.log(findMax([3, 5, 2, 8, 1]));
+console.log(findMin([3, 5, 2, 8, 1]));
 
 function sumArray(arr) {
-    let sum = 0
-    for (let i = 0; i < arr.length; i++) {
-        sum += arr[i]
-    }
-    return sum
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
 }
-console.log(sumArray([1, 2, 3, 4, 5]))
+console.log(sumArray([1, 2, 3, 4, 5]));
 
-function filterArray(arr, condition) {
-    const filtered = []
-    for (let i = 0; i < arr.length; i++) {
-        if (condition(arr[i])) {
-            filtered.push(arr[i])
-        }
+function filterArray(arr) {
+  const filtered = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      filtered.push(arr[i]);
     }
-    return filtered
+  }
+  return filtered;
 }
-console.log(filterArray([1, 2, 3, 4, 5], n % 2 === 0))
+console.log(filterArray([1, 2, 3, 4, 5]));
 
 function factorial(n) {
-    let result = 1
-    for (let i = 2; i <= n; i++) {
-        result *= i
-    }
-    return result
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
 }
-console.log(factorial(5))
+console.log(factorial(5));
 
 function isPrime(n) {
-    if (n <= 1) return false
-    for (let i = 2; i <= Math.sqrt(n); i++) {
-        if (n % i === 0) return false
-    }
-    return true
+  if (n <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
 }
-console.log(isPrime(7))
-console.log(isPrime(10))
+console.log(isPrime(7));
+console.log(isPrime(10));
 
 function fibonacci(n) {
-    const sequence = [0, 1]
-    for (let i = 2; i < n; i++) {
-        sequence.push(sequence[i - 1] + sequence[i - 2])
-    }
-    return sequence
+  const sequence = [0, 1];
+  for (let i = 2; i < n; i++) {
+    sequence.push(sequence[i - 1] + sequence[i - 2]);
+  }
+  return sequence;
 }
 
-console.log(fibonacci(5))
-
-
-
+console.log(fibonacci(5));
